3932b616daf374c5b5368419f5f16e3dc714d445
Nikita Sinitsa <ua45tj@gmail.com>
2016-03-01 17:01:39 +0300

    Updated files with proper content and file names

--- src/android/SampleText.java
+++ src/android/SampleText.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class sample-text extends CordovaPlugin {
+public class SampleText extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
